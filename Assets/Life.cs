1a98ea3f5882db46db1f2cc9177fed3f2b625a95
john <john@primitive.io>
2024-03-24 15:27:05 -0700

    parse newick tree

---
+++ Assets/Life.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    void Start()
+    {
+        TextAsset newickText = Resources.Load<TextAsset>("TimetreeOfLife2009");
+
+        TreeNode tree = NewickTreeParser.ParseNewick<TreeNode>(
+            newickText.text,
+            (label, children, distance, features) =>
+                new TreeNode(label, children, distance, features));
+
+        tree.Print();
+    }
+}
